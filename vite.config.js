84d5f41dd392a104020593851dccaf4c588c1050
mikhenaff <109875471+mikhenaff@users.noreply.github.com>
2023-11-03 23:07:16 +0100

    deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // fix deployment
-  base: "/wheather-app-react/",
+  base: "/weather-app-react/",
 });
